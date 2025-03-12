67baa07c2f289b57be97385dd55737f119a73e49
Carl Kesselman <carl@isi.edu>
2025-03-12 18:32:37 -0400

    Testing.

--- scripts/test.py
+++ scripts/test.py
@@ -11,6 +11,13 @@ except PackageNotFoundError:
     pass
 
 #ml_instance = DemoML()
+# content of my_module/__init__.py
+
+from pathlib import Path
+
+# you can use os.path and open() as well
+__version__ = Path(__file__).parent.joinpath("VERSION").read_text()
+
 
 if __name__ == "__main__":
     print(__version__)
