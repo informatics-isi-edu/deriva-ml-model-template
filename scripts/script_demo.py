97cbd00d822a6b474ee55eb532ee5af97e644d8f
Carl Kesselman <carl@isi.edu>
2025-03-20 10:21:16 -0700

    Comments.

--- scripts/script_demo.py
+++ scripts/script_demo.py
@@ -4,6 +4,7 @@ This file is a template for how to set up a stand-alone script to execute a mode
 from deriva_ml import DerivaML, ExecutionConfiguration, DatasetSpec, MLVocab, Execution
 from deriva.core import BaseCLI
 from typing import Optional
+
 datasets = []
 models = []
 
@@ -26,20 +27,23 @@ class DerivaDemoCLI(BaseCLI):
 
         self.deriva_ml = DerivaML(hostname, catalog_id)  # This should be changed to the domain specific class.
 
+        # Create a workflow instance for this specific version of the script.  Return an existing workflow if one is found.
         self.deriva_ml.add_term(MLVocab.workflow_type, "Demo Notebook", description="Initial setup of Model Notebook")
         workflow_rid = self.deriva_ml.create_workflow('demo-workflow', 'Demo Notebook')
 
+        # Create an execution instance that will work with the latest version of the input datasets.
         config = ExecutionConfiguration(
-            datasets=[DatasetSpec(rid=dataset, version=self.deriva_ml.dataset_version(dataset)) for dataset in datasets],
+            datasets=[DatasetSpec(rid=dataset, version=self.deriva_ml.dataset_version(dataset)) for dataset in
+                      datasets],
             assets=models,
             workflow=workflow_rid
         )
         self.execution = self.deriva_ml.create_execution(config)
-        with self.execution as _e:
-            self.do_stuff()
+        with self.execution as e:
+            self.do_stuff(e)
         self.execution.upload_execution_outputs()
 
-    def do_stuff(self):
+    def do_stuff(self, execution: Execution):
         """Put your model here"""
         pass
 
